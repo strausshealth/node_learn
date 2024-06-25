5527600451197d403530adf21f2e5dba92e4c4f9
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-06-25 16:17:24 +0530

    Done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Hello');
+// console.log('Hello');
+
+// const fs = require('fs');
+// fs.writeFileSync('hello2.txt','welcome')
+const fs = require('fs').writeFileSync;
+fs('sss.txt','gssg');
+// console.log(__dirname);
+// console.log(__filename);
