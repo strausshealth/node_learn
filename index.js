2c302f403e857d12f89042ca8d0c2e8cfaa6572c
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-06-25 16:42:34 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Hello');
+const http = require('http');
+
+// http.createServer((req, resp) => {
+//     resp.write('<h1>hello are u here</h1>');
+//     resp.end();
+// }).listen(4500);
+
+// Normal function
+// function data(req, resp) {
+//     resp.write('<h1>hello are u here</h1>');
+//     resp.end();
+// }
+
+// Aerro Function
+const data = (req, resp) => {
+    resp.write('<h1>hello 123 are u here</h1>');
+    resp.end();
+}
+http.createServer(data).listen(4500);
+
+
+// unother example of aerro and normal function
+// function test(a) {
+//     return a * 100;
+// }
+
+// const test = (a) => a * 100;
