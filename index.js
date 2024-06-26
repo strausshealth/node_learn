0ecd764751088a5fb7dfa2d0fef36ce7be39ccef
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-06-26 11:36:46 +0530

    done

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hello');
+const fs = require('fs');
+const path = require('path');
+const dirPath = path.join(__dirname, 'files');
+// fs.writeFileSync('apple.txt','this is apple file');
+
+// for (let i = 0; i < 5; i++) {
+//     fs.writeFileSync(dirPath + '/hello' + i + '.txt', 'simple text')
+// }
+
+fs.readdir(dirPath, (err, files) => {
+    files.forEach((item) => {
+        console.log(item);
+    })
+})
