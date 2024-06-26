a2862df43ca8ad7c5086efe032ab756bbba9d7e1
Gaurang Parante <gaurang.parante@strausshealthcare.com>
2024-06-26 10:35:30 +0530

    done

---
+++ data.js
@@ -0,0 +1,6 @@
+const data = [
+    { name: 'Gaurang', email: 'gmail@email.com' },
+    { name: 'dhaval', email: 'dhaval@email.com' },
+    { name: 'bhavesh', email: 'bhavesh@email.com' }
+];
+module.exports = data;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('Hello');
+const http = require('http');
+const data = require('./data');
+
+http.createServer((req, resp) => {
+    resp.writeHead(200, { 'Content-Type': 'applicatio\json' });
+    // resp.write(JSON.stringify({ name: 'Gaurang', email: 'gmail@email.com' }));
+    resp.write(JSON.stringify(data));
+    resp.end();
+}).listen(4500);
